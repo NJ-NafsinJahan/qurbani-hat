9d35a86437a846b824b7d16d78cc3004ad1fa056
NJ-NafsinJahan <nj.nafsinjahan@gmail.com>
2026-05-04 22:29:50 +0600

    check

--- src/components/shared/WhyUs.jsx
+++ src/components/shared/WhyUs.jsx
@@ -17,7 +17,7 @@ const WhyUsPage = () => {
           </p>
         </div>
       </div>
-      <div className=" grid grid-cols-3 gap-5">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {/* div 1 */}
         <div className="">
           <div className="card bg-base-100 w-96 shadow-sm">
